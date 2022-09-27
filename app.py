5229a07feb264d9b4406a108fbe5723aedbd1d99
Beatrix-droid <amilcare8@gmail.com>
2022-09-27 13:45:55 +0100

    second styling commit

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
-
+from paint import get_surface_area
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
+    if request.method == "POST":
+        pass
+
     return render_template("index.html")
 
 
